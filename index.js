a436b3021b2021928dad4a61378f85e1d79fdd09
unknown <dani.00rn@gmail.com>
2025-05-03 16:20:54 -0300

    adicionando condicionais a função.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Ocultar o card
 let card = document.getElementById('card').style.setProperty('display','none','important')
-// Mostra o card com as informções do placeholdera
+// Mostra o card com as informções do placeholder
 function Pesquisa(){
 let card = document.getElementById('card').style.setProperty('display','block','important')
-let busca = document.getElementById('form').value
+let busca = document.getElementById('form').value.trim()
 let teste = `http://localhost:3000/animais`
 console.log(teste)
 
@@ -13,25 +13,46 @@ fetch(teste)
 })
 
 .then(function(data){
-    console.log(data[0])
-    console.log(data[0].nome)
-    console.log(data[0].tipo)
-    console.log(data[0].descricao)
-    console.log(data[0].imagem)
+    console.log(data)
+    console.log(data.nome)
+    console.log(data.tipo)
+    console.log(data.descricao)
+    console.log(data.imagem)
 
     let nome = document.getElementById('nome')
     let tipo = document.getElementById('tipo')
     let descricao = document.getElementById('descricao')
     let imagem = document.getElementById('imagem')
 
-    nome.innerHTML = data[0].nome
-    tipo.innerHTML = data[0].tipo
-    descricao.innerHTML = data[0].descricao
-    imagem.src = data[0].imagem
+    nome.innerHTML = data.nome
+    tipo.innerHTML = data.tipo
+    descricao.innerHTML = data.descricao
+    imagem.src = data.imagem
+
+    if (busca === "") {
+        alert("Por favor, digite algo.");
+        document.getElementById('card').style.setProperty('display','none','important')
+    
+      }
 
 
+ let resultado = data.find( animal => animal.nome.toLowerCase().includes(busca.toLowerCase()))
+ 
 
+ if(resultado){
+
+    nome.innerHTML = resultado.nome
+    tipo.innerHTML = resultado.tipo
+    descricao.innerHTML = resultado.descricao
+    imagem.src = resultado.imagem
+ } else{
+    alert('Animal não encontradado!')
+    document.getElementById('card').style.setProperty('display','none','important')
+    
+ }
+ 
 })
+
 
 }
 
